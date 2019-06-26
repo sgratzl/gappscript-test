bc785d9f21ebdfdf30c23b59bad66ee6b23e08a7
Samuel Gratzl <samuel_gratzl@gmx.at>
2019-06-26 11:26:56 +1000

    working on gas plugins

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,60 @@ import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
 import builtins from 'rollup-plugin-node-builtins';
 
+// based on https://github.com/bengsfort/rollup-plugin-generate-html-template/
+
+import {readFile} from 'fs-extra';
+
+function wrapHTML(options = {}) {
+  const DEFAULT_TEMPLATE = `<!DOCTYPE html>
+  <html>
+  <head>
+  </head>
+  <body>
+  </body>
+  </html>`;
+  const template = options.template;
+  return {
+    name: 'inline-html',
+    renderChunk: (code, chunk, chunkOptions) => {
+      if (!chunk.isEntry) {
+        return null;
+      }
+      return Promise.resolve(template ? readFile(template).then((b) => b.toString('utf8')) : DEFAULT_TEMPLATE).then((tmpl) => {
+        const bodyCloseTag = tmpl.lastIndexOf('</body>');
+        // Inject the script tags before the body close tag
+        return [
+          tmpl.slice(0, bodyCloseTag),
+          `<script>${code}</script>\n`,
+          tmpl.slice(bodyCloseTag, tmpl.length),
+        ].join('');
+      });
+    },
+  };
+}
+
+function gaswrapper(options = {}) {
+  return {
+    name: 'gaswrapper',
+    renderChunk: (code, chunk, chunkOptions) => {
+      if (!chunk.isEntry) {
+        return null;
+      }
+      return `
+var _impl = (function() {
+  var exports = {};
+  ${code.replace(/\n/gm, '\n  ').trim()}
+  return exports;
+})();
+// generate wrapper for all exports
+${chunk.exports.map((f) => `function ${f}() {
+  return _impl.${f}.apply(this, arguments);
+}`).join('\n\n')}`;
+    }
+  };
+}
+
+
 export default [{
   input: './frontend/src/index.ts',
   output: {
@@ -14,8 +68,11 @@ export default [{
     resolve({
       preferBuiltins: true
     }),
-    typescript()
-	]
+    typescript(),
+    wrapHTML({
+      template: './frontend/src/index.html'
+    })
+  ]
 }, {
   input: './backend/src/index.ts',
   output: {
@@ -27,6 +84,7 @@ export default [{
     resolve({
       preferBuiltins: false
     }),
-		typescript()
-	]
+    typescript(),
+    gaswrapper()
+  ]
 }]
